68b1b8a76add8e59376f6abfb4a7e0313acd42e5
Zach Reizner <zach297@gmail.com>
2016-02-25 23:44:44 -0800

    add build, testing, and ci

---
+++ test/catch_main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
+
+#include <sodium.h>
+
+int main(int argc, char **argv)
+{
+    if (sodium_init() == -1) {
+        std::cerr << "Failed to initialize libsodium" << std::endl;
+        return 1;
+    }
+
+    Catch::Session session;
+    int ret = session.applyCommandLine(argc, argv);
+    if (ret)
+        return ret;
+
+    session.run();
+
+    return 0;
+}
